12987d79e0be7d01653c20aadc6d9f3649d6ece3
Naved <s.syed.naved12@gmail.com>
2023-06-24 22:49:49 +0530

    updating app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
-import pymongo 
 
 app = Flask(__name__)
 
@@ -73,11 +72,6 @@ def index():
                           "Comment": custComment}
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
-            client=pymongo.MongoClient("mongodb+srv://snaved12:Abd@69877@cluster0.fzoavlg.mongodb.net/?retryWrites=true&w=majority")
-            db=client['review_scrap']
-            review_col=db['review_scrap_data']
-            review_col.insert_many(reviews)
-
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             logging.info(e)
@@ -89,4 +83,4 @@ def index():
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=4000)
+    app.run(host="0.0.0.0",port=3000)
